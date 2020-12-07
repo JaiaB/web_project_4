47a77609b30f9194d14c2b04bc9878350165ddd5
Jaia B <konbae@ymail.com>
2020-12-07 13:26:48 -0500

    ready for revision

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,27 @@
 //Modals
-const modal = document.querySelector ('.modal');
-const form = document.querySelector ('.form');
-const newCardModal = document.querySelector('.modal__open_new-card');
-const imageModal = document.querySelector('.modal__open-image');
-
+const modal = document.querySelector(".modal");
+const form = document.querySelector(".form");
+const newCardModal = document.querySelector(".modal__open_new-card");
+const imageModal = document.querySelector(".modal__open-image");
+const profileForm = document.querySelector(".form_profile");
 
 //Buttons
-const editProfileButton = document.querySelector('.button_edit');
-const closeFormButton = document.querySelector('.modal__close-button');
-const addCardButton = document.querySelector('.button_add');
-const closeAddCardButton = document.querySelector('.modal__close-button_new-card');
-const closeImageModalButton = document.querySelector('.modal__close-button_image-modal');
+const editProfileButton = document.querySelector(".button_edit");
+const closeFormButton = document.querySelector(".modal__close-button");
+const addCardButton = document.querySelector(".button_add");
+const closeAddCardButton = document.querySelector(".modal__close-button_new-card");
+const closeImageModalButton = document.querySelector(".modal__close-button_image-modal");
 
 //Profile section info change
-const profileName = document.querySelector('.profile__name');
-const profileDescription = document.querySelector('.profile__description');
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 
-//Form Inputs
-const formName = document.querySelector('.form__input_type_name');
-const formDescription = document.querySelector('.form__input_type_description');
-
-//Smooth transition
-function smoothTransition(){
-  modal.oncommand.style.opacity = "1";
-}
+//Form Inputs Profile Edition
+const formName = form.querySelector(".form__input_type_name");
+const formDescription = form.querySelector(".form__input_type_description");
 
 //Open modal 
-function toggleModal () {
+function toggleModal() {
   modal.classList.toggle('modal__open');
 }
 
@@ -36,25 +31,24 @@ function toggleAddCard() {
 }
 
 //Open image modal 
-function openImageModal(){
+function openImageModal() {
   imageModal.classList.toggle('modal__open');
 }
 
-//Buttons functionality
+//Buttons functionality global scope
 editProfileButton.addEventListener('click', toggleModal)
 closeFormButton.addEventListener('click', toggleModal)
 addCardButton.addEventListener('click', toggleAddCard)
 closeAddCardButton.addEventListener('click', toggleAddCard)
 closeImageModalButton.addEventListener('click', openImageModal)
 
-
 //Refresh information in profile section
-function getUpdatedInfo () {
+function getUpdatedInfo() {
   formName.value = profileName.textContent;
   formDescription.value = profileDescription.textContent;
 }
 
-form.addEventListener('submit', function(e) {
+profileForm.addEventListener('submit', function (e) {
   e.preventDefault();
   profileName.textContent = formName.value;
   profileDescription.textContent = formDescription.value;
@@ -86,45 +80,83 @@ const initialCards = [
   },
   {
     name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    alt: "Lago di Braies"
   }
-]; 
+];
 
+//variables for the card form
+const cardForm = document.querySelector(".form_new-card");
+const createCardButton = document.querySelector(".form__button_create-card")
+//for createCard object, we take the name and link from the initial cards object list and we refer to the values so they are retrieved from the inputs
+function addImageHandler(event){
+  event.preventDefault();
+  const newCardElement = createCard({name: newCardName.value, link: newCardImageLink.value });
+  gallery.prepend(newCardElement);
+  cardForm.reset();
+  toggleAddCard();
+  //i had return newCardElement here
+}
+
+//event listener for addImageHandler. 
+createCardButton.addEventListener('click', addImageHandler);
+
+//Form Inputs Create New Card
+const newCardName = document.querySelector(".form__input_type_card-title");
+const newCardImageLink = document.querySelector(".form__input_type_url");
+//Card template section
 const cardTemplate = document.querySelector(".card-template").content.querySelector(".cards__gallery-item");
+
 //gallery will select the element in the DOM where we want to place the dynamic data.
 const gallery = document.querySelector(".cards__gallery");
 
+//elements used when opening image modal
+const modalImage = document.querySelector(".modal__image");
+const modalCaption = document.querySelector(".modal__caption");
 
-initialCards.forEach(data => {
+function createCard(data){
   const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector(".cards__image");
   const cardTitle = cardElement.querySelector(".cards__text");
   const cardLikeButton = cardElement.querySelector(".cards__button_like_inactive");
   const cardDeleteButton = cardElement.querySelector(".cards__button_delete");
 
-  //elements to work with data
+  //dynamic data that renders image and name of card
   cardTitle.textContent = data.name;
   cardImage.src = data.link;
-  //*cardImage.style.backgroundImage=`url(${data.link})`; if i use a div for the image instead of the img tag
   
-  cardLikeButton.addEventListener('click', (event)=> {
-    event.target.classList.toggle('cards__button_like_active');
-    //console.log(EventSource);
+  cardLikeButton.addEventListener('click', (event) => {
+    event.target.classList.toggle("cards__button_like_active");
   });
+
+  //Delete card
+  function deleteCard(){
+   gallery.removeChild(cardElement);
+ }
 
   cardDeleteButton.addEventListener('click', () => {
-  //handlerCardDeleteClick()
+    deleteCard();
+    //console.log('card was deleted');
   });
 
-  cardImage.addEventListener('click', (event)=> {
-  //Open image modal
-  const modalImage = document.querySelector('.modal__image');
-  const modalCaption = document.querySelector('.modal__caption');
-   openImageModal();
-   event.target.classList.toggle('modal__open-image');
-   modalImage.src = data.link;
-   modalCaption.textContent = data.name;
+  //open image modal when clicking on the image
+  cardImage.addEventListener('click', (event) => {
+    event.preventDefault();
+    modalImage.src = data.link;
+    modalCaption.textContent = data.name;
+    modalImage.alt = data.name;
+    openImageModal();
   });
 
-  gallery.prepend(cardElement); 
+  return cardElement;
+}
+
+//this inserts the created card and initial cards into the gallery DOM
+function insertImage(data) {
+  gallery.prepend(createCard(data));
+}
+
+//this function calls the insertImage for every object in the list
+initialCards.forEach((data) => {
+  insertImage(data);
 });
