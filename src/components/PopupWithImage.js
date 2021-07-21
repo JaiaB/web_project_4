31c4ce86a0437ded9c190337985647fea7d461b9
Jaia B <konbae@ymail.com>
2021-07-20 23:29:14 -0400

    PopupWithImage ready for review

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,10 +1,15 @@
 import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup{
-    open(link, caption) {
-        this._popupSelector.querySelector('.modal__image').style.backgroundImage = `url(${link})`;
-        this._popupSelector.querySelector('.modal__caption').textcontent = caption;
+    constructor(popupSelector){
+        super(popupSelector);
+        this._popupImage = this._popupSelector.querySelector('.modal__image');
+        this._popupCaption = this._popupSelector.querySelector('.modal__caption');
+    }
 
+    open(link, caption){
+        this._popupImage.src = link;
+        this._popupCaption.textContent = caption;
         super.open();
     }
 };
