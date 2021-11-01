8377dacf53625af1919787e1e1b06a6fb167e2bf
Jaia B <konbae@ymail.com>
2021-11-01 03:15:14 -0400

    Revert "fixing Card"
    This reverts commit 11d78d787959bd1b56d254f3a430bc8412c7aabc.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,17 @@
 export default class Card {
-  constructor({data, handleCardClick}, templateSelector) {
+  constructor(data, templateSelector, handleCardClick) {
     this._link = data.link; //image data
     this._name = data.name; //name data
     this._templateSelector = templateSelector; //the template element (not the instance)
-    this._handleCardClick = handleCardClick; //function to open image modal
+    this._handleCardClick = handleCardClick;
   }
   
   _getCardTemplate(){
-    const cardTemplate = document.querySelector(this._templateSelector)
-      .content.querySelector(".cards__gallery-item")
-      .cloneNode(true); //we need to place this variable here in order to find the elements below in the generate card. 
-      //first we find the template, and then we return it.
+    const cardTemplate = document
+    .querySelector(this._templateSelector)
+    .content.querySelector(".cards__gallery-item")
+    .cloneNode(true);
+
     return cardTemplate;
   }
   
@@ -23,7 +24,7 @@ export default class Card {
     this._cardElement = null;
   }
   
-  _setEventListeners(){
+  _setEventListeners(){  
     this._cardElement.querySelector(".cards__button_like_inactive").addEventListener('click', (evt)=>{
       this._likeCard(evt);
     });
@@ -32,24 +33,19 @@ export default class Card {
       this._deleteCard(this._cardElement);
     });
 
-    //handler to render image and name when clicking on the card image.
+    //handler to render image and name when clicking on the image modal.
     this._cardElement.querySelector(".cards__image").addEventListener('click', () =>{
-      this._handleCardClick();
-    });
+      this._handleCardClick;
+    } );
   }
   
-  generateCard(){ //is the same as createCard
-    //we can store this._cardElement here instead of the constructor
+  generateCard(){ //is the same as createCard in index
     this._cardElement = this._getCardTemplate();
   
     //now we search the elements inside this._cardElement
-    const cardImage = this._cardElement.querySelector(".cards__image");
-    const cardTitle = this._cardElement.querySelector(".cards__text");
-  
-    //dynamic data that renders image and name of card
-    cardTitle.textContent = this._name;
-    cardImage.src = this._link;
-    cardImage.alt = this._name;
+    //dynamic data rendering name and image of card
+    this._cardElement.querySelector(".cards__image").src = this._link;
+    this._cardElement.querySelector(".cards__text").textContent = this._name;
   
     //we have to call the private method of _setEventListeners because its needed for the card to be generated properly. 
     this._setEventListeners();
